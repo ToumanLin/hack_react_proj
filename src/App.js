a351117b9989d8b6ef5f0856cfe664ff6df75055
Matthew Lee <matthew.c.lee@gmail.com>
2025-06-27 10:27:01 -0700

    adding  example code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,77 @@
-import logo from './logo.svg';
+import logo from './MatthewLee.jpg';
 import './App.css';
+import {useState, useEffect} from 'react';
+
+
+
+//  function LikeButton() {
+//     const [likes, setLikes] = useState(0);
+
+//     function likeFunction() {
+//       setLikes(likes + 1)
+//     }
+
+//     return (
+//       <div>
+//       <button onClick={likeFunction}>Like my page!</button>
+//          <p>Likes: {likes}</p>
+//       </div>
+//     )
+//  }
+
+function LikeButton(props) {
+    const [likes, setLikes] = useState(0);
+
+    function likeFunction() {
+      setLikes(likes + 1)
+    }
+
+    function changeTitleColor() {
+    const titleElement = document.querySelector('.Title');
+      if (likes >= 5) {
+        titleElement.style.color = 'red';
+      } 
+    }
+
+  useEffect(changeTitleColor, [likes]);
+    return (
+      <div>
+      <button onClick={likeFunction}>{props.description}</button>
+         <p>{props.count}: {likes}</p>
+      </div>
+    )
+ }
 
 function App() {
+  // const [likes, setLikes] = useState(0); // Used in example 2
+
+  // function likeFunction() {
+  //   setLikes(likes + 1);  
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="Title">
+          Hi, I'm Matthew
         </p>
+        <img src={logo} className="App-logo" alt="logo" />
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/matthewlee22"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is my github!
         </a>
+         {/* <button>Like my page!</button>  */}
+         {/* <div>
+            <button onClick={likeFunction}>Like my page!</button>
+            <p>Likes: {likes}</p>
+         </div> */}
+         {/* <LikeButton /> */}
+         <LikeButton description="Follow my page!" count="Follows" />
       </header>
     </div>
   );
